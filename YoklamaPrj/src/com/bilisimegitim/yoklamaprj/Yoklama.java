a0ea02825f35bf73e0d495f34678d50816236690
Bemdell7 <Bemdell7@DESKTOP-V6RE7BG>
2018-04-29 16:45:00 +0300

    ilk class yoklama.java eklendi

---
+++ YoklamaPrj/src/com/bilisimegitim/yoklamaprj/Yoklama.java
@@ -0,0 +1,16 @@
+package com.bilisimegitim.yoklamaprj;
+
+
+public class Yoklama {
+
+	public static void main(String[] args) {
+
+		String isimler [] = {"Bora","Esra","Berna","Sertaç","Boğaç","Gülşah","Yunus", "Taner"} ;
+		for (int i = 0; i < isimler.length; i++) {
+			System.out.println(isimler[i]);
+		}
+		
+				
+	}
+
+}
